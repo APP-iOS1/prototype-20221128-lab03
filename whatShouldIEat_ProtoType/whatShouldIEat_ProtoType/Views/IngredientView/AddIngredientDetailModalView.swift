de578a71d97e0cea84be92fe8481bf75916e5b94
valseLee <sollleky72@gmail.com>
2022-12-01 13:23:49 +0900

    add: 디테일모달뷰 추가

---
+++ whatShouldIEat_ProtoType/whatShouldIEat_ProtoType/Views/IngredientView/AddIngredientDetailModalView.swift
@@ -0,0 +1,20 @@
+//
+//  AddIngredientDetailModalView.swift
+//  whatShouldIEat_ProtoType
+//
+//  Created by 이승준 on 2022/11/30.
+//
+
+import SwiftUI
+
+struct AddIngredientDetailModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddIngredientDetailModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddIngredientDetailModalView()
+    }
+}
