d90af734013a7a8c1458101b6dfa5987dd4969cf
valseLee <sollleky72@gmail.com>
2022-12-02 14:42:53 +0900

    add: 재료 추가를 위한 상세 셀 뷰 파일 생성

---
+++ whatShouldIEat_ProtoType/whatShouldIEat_ProtoType/Views/IngredientView/AddIngredientComponent/EachAddIngredientViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  EachAddIngredientViewCell.swift
+//  whatShouldIEat_ProtoType
+//
+//  Created by 이승준 on 2022/12/02.
+//
+
+import SwiftUI
+
+struct EachAddIngredientViewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EachAddIngredientViewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        EachAddIngredientViewCell()
+    }
+}
